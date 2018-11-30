382bd884954605f23c10141f07dec0d16be22678
sitifatma <sitifatmawati852@gmail.com>
2018-11-30 08:46:52 +0700

    Menambah tampilan edit text

---
+++ app/src/main/java/com/example/nurad/finalprojejtandroid/Edit_Profill.java
@@ -0,0 +1,17 @@
+package com.example.nurad.finalprojejtandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Edit_Profill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__profill);
+    }
+
+    public void selesai(View view) {
+    }
+}
